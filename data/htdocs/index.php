7f96649accc293ac49a2071de97c5cf98ec43815
Sunny <volokonskiy52@gmail.com>
2019-11-28 12:30:08 +0200

    Create connection with table

--- data/htdocs/index.php
+++ data/htdocs/index.php
@@ -1,2 +1,30 @@
 <?php
-phpinfo();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDBPDO";
+
+//Creat connection
+try {
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Creat table
+    $sql = "CREATE TABLE MyList (
+   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+   firstName VARCHAR(30) NOT NULL,
+   lastName VARCHAR(30) NOT NULL,
+   Phone VARCHAR(30) NOT NULL,
+   email VARCHAR(50),
+   reg_date TIMESTAMP
+   )";
+
+    $connection->exec($sql);
+    echo "Таблица MyList создана успешно";
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+// Close connection
+$connection = null;
